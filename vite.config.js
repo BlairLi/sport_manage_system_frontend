96db990ef97095f7cebcf061846be28b59dabfee
BlairLi <1579193739@qq.com>
2024-06-19 17:21:06 -0400

    testing that use vite-plugin-vercel to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vercel from 'vite-plugin-vercel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [vercel()],
+  vercel: {
+    // optional configuration options, see "Advanced usage" below for details
+  },
   // server: {
   //   host: '0.0.0.0', // Specify host
   //   port: 5173, // Specify port
