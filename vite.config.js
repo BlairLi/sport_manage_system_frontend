bf3b6faac8c5aed0cbdb3a7df63f685f74391a05
BlairLi <1579193739@qq.com>
2024-06-19 17:12:45 -0400

    try to see wheather this change could solve the production problem

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Specify host
-    port: 5173, // Specify port
-    strictPort: true, // Fail if the port is already in use
-    open: true, // Open the browser when the server starts
-  },
+  // server: {
+  //   host: '0.0.0.0', // Specify host
+  //   port: 5173, // Specify port
+  //   strictPort: true, // Fail if the port is already in use
+  //   open: true, // Open the browser when the server starts
+  // },
 })
